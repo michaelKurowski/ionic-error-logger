751a79bf6a2df0fbf813a22bfad12919702d900f
Michael Kurowski <Byamarro@gmail.com>
2017-05-29 16:25:27 +0200

    Major fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-module.exports = function (message, throwToConsole = false) {
-	const errorLog = `[${new Date()}] /${endpoint} - ${err}`
+module.exports = function (whereHappened, message, throwToConsole = false) {
+	const errorLog = `[${new Date()}] ${whereHappened} - ${message}`
 	fs.writeFile('logs.txt', errorLog, err => {
 		if (throwToConsole) console.log(errorLog)
 	})
